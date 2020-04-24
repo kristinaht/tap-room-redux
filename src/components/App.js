36645a516356c0d43818ae6caaba0308069b29a9
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-04-24 09:05:40 -0700

    import Header, WineControl and Footer in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import WineControl from './WineControl';
+import Footer from './Footer';
 
 function App() {
   return (
     <React.Fragment>
-      
+      <Header />
+      <WineControl />
+      <Footer />
     </React.Fragment>
   );
 }
