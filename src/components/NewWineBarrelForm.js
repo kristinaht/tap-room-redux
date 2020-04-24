4c94b6e7aab0713b19c1f7e4bcdac7a15eec3c50
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-04-24 09:51:29 -0700

    fix typo in WineBarrel.js

---
+++ src/components/NewWineBarrelForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NewWineBarrelForm(props){
+  return(
+    <React.Fragment>
+      <h1>Imagine a form here.</h1>
+    </React.Fragment>
+  )
+}
+
+export default NewWineBarrelForm;
+
+NewWineBarrelForm.propTypes = {
+
+}
---
+++ src/components/WineBarrel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function WineBarrel(props){
+  return(
+    <p>White Wine</p>
+  );
+}
+
+export default WineBarrel;
+
+WineBarrel.propTypes={
+
+}
