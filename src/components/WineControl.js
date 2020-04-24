c7619cc35c9621e892428eeea90187cd45e8d301
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-04-24 10:00:23 -0700

    add some basic conditional rendering, button click handler for toggling between components

--- src/components/WineControl.js
+++ src/components/WineControl.js
@@ -6,14 +6,33 @@ class WineControl extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state={};
+    this.state={
+      formVisibleonPage: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState =>({
+      formVisibleonPage: !prevState.formVisibleonPage
+    }));
   }
 
   render(){
+    let currentlyVisibleState=null;
+    let buttonText=null;
+
+    if(this.state.formVisibleonPage){
+      currentlyVisibleState=<NewWineBarrelForm />;
+      buttonText="Return to Wine Barrel List";
+    } else {
+      currentlyVisibleState=<WineBarrelList />;
+      buttonText="Add Wine Barrel";
+    }
+
     return(
       <React.Fragment>
-        <WineBarrelList />
-        <NewWineBarrelForm />
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     )
   }
