25e8f0a7679d8b4380ddd32f7a27665174949f6d
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-04-24 10:18:25 -0700

    add a form to NewWineBarrelForm for adding new barrels

--- src/components/NewWineBarrelForm.js
+++ src/components/NewWineBarrelForm.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { v4 } from 'uuid';
 import PropTypes from 'prop-types';
 
 function NewWineBarrelForm(props){
+
+  function handleNewWineBarrelFormSubmission(event) {
+    //something goes here
+  }
+
   return(
     <React.Fragment>
-      <h1>Imagine a form here.</h1>
+      <form onSubmit={handleNewWineBarrelFormSubmission}>
+        <input
+        type='text'
+        name='name'
+        placeholder='Wine name'/>
+              <input
+        type='text'
+        origin='origin'
+        placeholder='State/Region'/>
+        <input
+        type='text'
+        liters='liters'
+        placeholder='Liters'/>
+        <button type='submit'>Add new barrel</button>
+      </form>
     </React.Fragment>
   )
 }
