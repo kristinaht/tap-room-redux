4a35a76537b0dbe377f44e4ee6f4000bbef04e13
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-05-01 09:24:28 -0700

    pass the SELECT_WINE_BARREL action test

--- src/reducers/select-wine-barrel-reducer.js
+++ src/reducers/select-wine-barrel-reducer.js
@@ -1,3 +1,9 @@
 export default (state={ selectedWineBarrel: null }, action) => {
-  
+  const { id } = action;
+  switch(action.type) {
+    case 'SELECT_WINE_BARREL':
+      const newState = { ...state};
+      const selectedState = newState[id];
+      return selectedState;
+  }
 }
