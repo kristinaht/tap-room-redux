b09baa5cf77e889a631f90d863b9098ca0a33089
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-05-01 12:13:42 -0700

    write and fail test for deleteWineBarrel action

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -2,7 +2,7 @@ import * as actions from './../actions';
 
 describe('help queue actions', () => {
   it('deleteWineBarrel should create DELETE_WINE_BARREL action', () => {
-    expect(actions.deleteTicket(1)).toEqual({
+    expect(actions.deleteWineBarrel(1)).toEqual({
       type: 'DELETE_WINE_BARREL',
       id: 1
     });
